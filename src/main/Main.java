e93d7a555b60cfae22b0b08e5548517e195a35d0
AGL-12” <aglgg21@gmail.com>
2025-09-23 10:43:42 +0200

    añadiendo cosas

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,14 +5,25 @@
  */
 package main;
 
-import controlador.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    
-    InterfazDao dao = DaoImplemnetBD();
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/vista/Vistalogin.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
-    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
